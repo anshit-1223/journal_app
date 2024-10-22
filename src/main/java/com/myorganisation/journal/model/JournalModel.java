456c2cd6fa8c1ee3622c074e57b44fd42b539b6f
anshit-1223 <81354203+anshit-1223@users.noreply.github.com>
2024-10-22 22:03:29 +0530

    Add Lombok

--- src/main/java/com/myorganisation/journal/model/JournalModel.java
+++ src/main/java/com/myorganisation/journal/model/JournalModel.java
@@ -1,11 +1,13 @@
 package com.myorganisation.journal.model;
 
+import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
 
 @Document(collection = "journal_entries")
+@Data
 public class JournalModel {
 
     @Id
@@ -14,36 +16,5 @@ public class JournalModel {
     private String description;
     private LocalDateTime dateTime;
 
-    public LocalDateTime getDateTime() {
-        return dateTime;
-    }
-
-    public void setDateTime(LocalDateTime dateTime) {
-        this.dateTime = dateTime;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
 
 }
