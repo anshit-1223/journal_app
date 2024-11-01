b5968e40c33f7032f9e9a2318a63585616b6c023
anshit-1223 <81354203+anshit-1223@users.noreply.github.com>
2024-11-01 21:03:36 +0530

    Created API

--- src/main/java/com/myorganisation/journal/controller/UserController.java
+++ src/main/java/com/myorganisation/journal/controller/UserController.java
@@ -1,4 +1,15 @@
 package com.myorganisation.journal.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
 public class UserController {
+
+    @GetMapping
+    public String getAllUser(){
+        return "Users List";
+    }
 }
