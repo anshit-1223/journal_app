943ef69d89b26e8c4ff1fd7c9592a3edab06860f
Anshit Verma <anshit1223@gmail.com>
2025-04-13 12:08:31 +0530

    code refactor

--- src/main/java/com/myorganisation/journal/controller/HealthCheck.java
+++ src/main/java/com/myorganisation/journal/controller/HealthCheck.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HealthCheck {
 
     @GetMapping("/health-check")
-    public String healthCheck(){
-        return "Server is Live";
+    public String healthCheck() {
+        return "Server is Live!!";
     }
 }
